a489c9afc4b6c0d0f94bd1ee41878125eba30776
Michael Wagner <michaelwagner42@googlemail.com>
2025-10-12 11:09:07 +0200

    wip

---
+++ installDependencies.sh
@@ -0,0 +1,5 @@
+
+# Install required collections
+# These will be installed to machine local default folders.
+ansible-galaxy collection install ansible.posix --upgrade
+ansible-galaxy collection install community.general --upgrade
